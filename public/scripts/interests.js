df3b656cc35a0b9e4c973583e2f6c0104a88fed4
Andrea  Piazza <apiazza023@gmail.com>
2018-11-23 23:09:36 -0800

    converted interests.js to ES6

--- public/scripts/interests.js
+++ public/scripts/interests.js
@@ -8,28 +8,24 @@ const signupSuccess = (json) => {
     saveStuff(tokenJson)
 }
 
-function onSuccess(response){
-    var googleMapRes = JSON.stringify(response)
-    var parsed = JSON.parse(googleMapRes)
-        console.log('success ', googleMapRes)
+onSuccess = (response) => {
+    let googleMapRes = JSON.stringify(response)
+    let parsed = JSON.parse(googleMapRes)
     
     //get longitude and latitude
-    var lat = parsed.results[0].geometry.bounds.northeast.lat
-    var lng = parsed.results[0].geometry.bounds.northeast.lng
+    let lat = parsed.results[0].geometry.bounds.northeast.lat
+    let lng = parsed.results[0].geometry.bounds.northeast.lng
+    
     Cookies.set("lng", lng)
     Cookies.set("lat", lat)
 }
 
-$(document).ready(function(){
-    console.log("cookie", Cookies.get('username')); // => 'value')
-    $('#location').on('submit',function(e){
+$(document).ready(() => {
+    $('#location').on('submit',(e) => {
         e.preventDefault();
-         let zipCodeData = $('#zipCode').val()
-         console.log(zipCodeData) 
-         console.log(zipCode)
+        let zipCodeData = $('#zipCode').val()
         
         Cookies.set("zipCode", zipCodeData);
-        console.log("cookie", Cookies.get('zipCode')); // => 'value') 
 
         let endpoint = `http://maps.googleapis.com/maps/api/geocode/json?address=13413&sensor=false&key=AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg`
         
@@ -38,48 +34,48 @@ $(document).ready(function(){
             method: 'GET',
             url: endpoint,
             success: onSuccess,
-            error: function(response){
-                console.log('Error:' + JSON.stringify(response))
+            error: (response) => {
+                console.log('ERROR:' + JSON.stringify(response))
             }
         })
     })
 })//end doc.ready
 
-var allInterests = [
-    "tech","family","hw","sf", "learning","photo","fd","writing","lc","music","move","lgbtq","film","sfg","beliefs","art","bc","dance","pets","hc","fb","cb","social"
+let allInterests = [
+    "tech", "family", "hw", "sf", "learning", "photo", "fd", "writing", "lc","music", "move", "lgbtq", "film", "sfg", "beliefs", "art", "bc", "dance","pets","hc", "fb", "cb", "social"
 ]
-var userInterests = [];
 
-for(let i = 0; i < allInterests.length; i++){
+let userInterests = [];
+
+for (let i = 0; i < allInterests.length; i++){
     let interest = allInterests[i]
-    document.getElementById(interest).onclick = function(){
+    document.getElementById(interest).onclick = () => {
         if ( this.checked ) {
             userInterests.push($(this).attr("data-id") );
         } else {
-        let index = userInterests.indexOf($(this).attr("data-id") )
+            let index = userInterests.indexOf($(this).attr("data-id") )
             userInterests.splice(index,1)
         }   
     };
 }
 
-$('form').submit(function(e) {
+$('form').submit((e) => {
     e.preventDefault()
-    console.log("cookie", Cookies.get('username')); // => 'value')
-    var username = Cookies.get('username') 
-    var id = $("data-id").val()
-    console.log("userInterests "+ userInterests)
+    let username = Cookies.get('username') 
+    let id = $("data-id").val()
+
     $.ajax({
         method: "PUT",
         url: "http://localhost:3000/interests",
         data: {
-            username: username, //pass user in from index.html, may use email instead
-            interests: userInterests //get from intrests.html
+            username: username, 
+            interests: userInterests
         },
-        success: ()=>{
-           $('#submitBtn').on('click',()=>{
-               window.location.replace('/profile')
+        success: () => {
+            $('#submitBtn').on('click', () => { 
+                window.location.replace('/profile')
             }) 
         },
-        error: console.log('Error - interests not submitted')
+        error: console.log('Error: Interests not submitted')
     }) 
 })
